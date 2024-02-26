a5f03a609ae0fe11be1e319bcfde0670d9f61bce
mightyloot <orkingua@gmail.com>
2024-02-26 10:32:02 +0200

    V1__create_db.sql changes

--- src/main/resources/db.migration/V1__create_db.sql
+++ src/main/resources/db.migration/V1__create_db.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS client(
     id BIGSERIAL PRIMARY KEY,
-    name varchar(200) NOT NULL CHECK (length(name) >= 3 and length(name <= 200))
+    name VARCHAR(200) NOT NULL CHECK (length(name) >= 3 and length(name <= 200))
 );
 
 CREATE TABLE IF NOT EXISTS planet(
-    id varchar(200) PRIMARY KEY NOT NULL CHECK (length(name) >= 1)  CONSTRAINT id_format_check CHECK (id ~ '^[A-Z0-9]+$'),
-    name varchar(500) NOT NULL CHECK (length(name) >= 1 and length(name <= 500))
+    id VARCHAR(200) PRIMARY KEY NOT NULL CHECK (length(name) >= 1)  CONSTRAINT id_format_check CHECK (id ~ '^[A-Z0-9]+$'),
+    name VARCHAR(500) NOT NULL CHECK (length(name) >= 1 and length(name <= 500))
 );
 
 CREATE TABLE IF NOT EXISTS ticket(
     id BIGSERIAL PRIMARY KEY,
     create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     client_id BIGINT REFERENCES client(id),
-    from_planet VARCHAR(200) references planet(id),
-    to_planet varchar(200) references planet(id)
+    from_planet VARCHAR(200) REFERENCES planet(id),
+    to_planet VARCHAR(200) REFERENCES planet(id)
 );
