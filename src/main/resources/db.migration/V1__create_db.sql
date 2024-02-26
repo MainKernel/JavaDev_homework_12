f5c18cb3cd88c00aa4989567a16f5ccaa7c99b67
mightyloot <orkingua@gmail.com>
2024-02-26 10:32:15 +0200

    V1__create_db.sql changes

--- src/main/resources/db.migration/V1__create_db.sql
+++ src/main/resources/db.migration/V1__create_db.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS client(
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(200) NOT NULL CHECK (length(name) >= 3 and length(name <= 200))
+    name VARCHAR(200) NOT NULL CHECK (length(name) >= 3 AND length(name <= 200))
 );
 
 CREATE TABLE IF NOT EXISTS planet(
     id VARCHAR(200) PRIMARY KEY NOT NULL CHECK (length(name) >= 1)  CONSTRAINT id_format_check CHECK (id ~ '^[A-Z0-9]+$'),
-    name VARCHAR(500) NOT NULL CHECK (length(name) >= 1 and length(name <= 500))
+    name VARCHAR(500) NOT NULL CHECK (length(name) >= 1 AND length(name <= 500))
 );
 
 CREATE TABLE IF NOT EXISTS ticket(
